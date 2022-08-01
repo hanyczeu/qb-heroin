2ceccfc659fe8397ba9cca1d341317b2147ea714
HanyCZeu <74603656+hanyczeu@users.noreply.github.com>
2022-08-01 08:04:34 +0200

    Update config.lua

--- qb-opium/config.lua
+++ qb-opium/config.lua
@@ -6,7 +6,7 @@ Config.Delays = {
 	ProcessOpium = 1000 * 1
 }
 
-Config.Pricesell = 700
+Config.Pricesell = 1150
 
 Config.MinPiecesWed = 1
 
@@ -23,7 +23,7 @@ Config.LicenseEnable = false -- enable processing licenses? The player will be r
 Config.GiveBlack = false -- give black money? if disabled it'll give regular cash.
 
 Config.CircleZones = {
-	PickupOpium = {coords = vector3(3265.01, -153.27, 18.6), name = 'blip_PickupOpium', color = 25, sprite = 496, radius = 30.0},
-	ProcessOpium = {coords = vector3(1208.68, -3115.82, 5.54), name = 'blip_ProcessOpium', color = 25, sprite = 496, radius = 100.0},
+	OpiumField = {coords = vector3(3265.01, -153.27, 18.6), name = 'blip_PickupOpium', color = 25, sprite = 496, radius = 30.0},
+	OpiumProcessing = {coords = vector3(1208.68, -3115.82, 5.54), name = 'blip_ProcessOpium', color = 25, sprite = 496, radius = 100.0},
 	DrugDealer = {coords = vector3(2461.11, 1589.51, 33.04), name = 'blip_DrugDealer', color = 6, sprite = 378, radius = 25.0},
 }
