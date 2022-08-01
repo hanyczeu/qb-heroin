66d7956024f9e83cc8e9c85cf088e252c2e061d8
HanyCZeu <74603656+hanyczeu@users.noreply.github.com>
2022-08-01 08:04:07 +0200

    Update server.lua

--- qb-opium/server/server.lua
+++ qb-opium/server/server.lua
@@ -1,80 +1,72 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+local itemcraft = 'markedbills'
 
-RegisterServerEvent('qb-opium:pickedUpOpium') --hero
-AddEventHandler('qb-opium:pickedUpOpium', function()
+RegisterServerEvent('qb-opiumpicking:pickedUpOpium', function()
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
 
-	    if 	TriggerClientEvent("QBCore:Notify", src, "You Got Some Opium.", "Success", 8000) then
+	    if 	TriggerClientEvent("QBCore:Notify", src, "Picked up some Opium!!", "Success", 1000) then
 		  Player.Functions.AddItem('opium', 1) ---- change this shit 
 		  TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['opium'], "add")
 	    end
-end)	
+end)
 
-RegisterServerEvent('qb-opium:processopium')
-AddEventHandler('qb-opium:processopium', function()
-	local src = source
-	local Player = QBCore.Functions.GetPlayer(src)
+RegisterServerEvent('qb-opiumpicking:processopium', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local opium = Player.Functions.GetItemByName("opium")
+      local plastic_baggie = Player.Functions.GetItemByName("plastic_baggie")
 
-	if Player.Functions.GetItemByName('opium') and Player.Functions.GetItemByName('plastic_baggie') then
-		local chance = math.random(1, 8)
-		if chance == 1 or chance == 2 or chance == 3 or chance == 4 or chance == 5 or chance == 6 or chance == 7 or chance == 8 then
-			Player.Functions.RemoveItem('plastic_baggie', 1)----change this
-			Player.Functions.RemoveItem('opium', 2)---change this
-			Player.Functions.AddItem('heroin', 1)----change this
-			TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['opium'], "remove")
-			TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['plastic_baggie'], "remove")
-			TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['heroin'], "add")
-			TriggerClientEvent('QBCore:Notify', src, 'Heroin Processed successfully', "success")  
-		end 
-	else
-		TriggerClientEvent('QBCore:Notify', src, 'You don\'t have the right items', "error") 
-	end
+    if opium ~= nil and plastic_baggie ~= nil then
+        if Player.Functions.RemoveItem('opium', 3) and Player.Functions.RemoveItem('plastic_baggie', 1) then
+            Player.Functions.AddItem('heroin', 1)-----change this
+            TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['opium'], "remove")
+            TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['plastic_baggie'], "remove")
+            TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['heroin'], "add")
+            TriggerClientEvent('QBCore:Notify', src, 'Opium Processed successfully', "success")  
+        else
+            TriggerClientEvent('QBCore:Notify', src, 'You don\'t have the right items', "error") 
+        end
+    else
+        TriggerClientEvent('QBCore:Notify', src, 'You don\'t have the right items', "error") 
+    end
 end)
 
 --selldrug ok
 
-RegisterServerEvent('qb-opium:selld2')
-AddEventHandler('qb-opium:selld2', function()
+RegisterServerEvent('qb-opiumpicking:selld', function()
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
 	local Item = Player.Functions.GetItemByName('heroin')
    
-	
-      
-	for i = 1, Item.amount do
-	if Item.amount >0 then
-	if Player.Functions.GetItemByName('heroin') then
-		local chance2 = math.random(1, 8)
+	if Item ~= nil and Item.amount >= 1 then
+		local chance2 = math.random(1, 12)
 		if chance2 == 1 or chance2 == 2 or chance2 == 9 or chance2 == 4 or chance2 == 10 or chance2 == 6 or chance2 == 7 or chance2 == 8 then
 			Player.Functions.RemoveItem('heroin', 1)----change this
 			TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['heroin'], "remove")
 			Player.Functions.AddMoney("cash", Config.Pricesell, "sold-pawn-items")
-			TriggerClientEvent('QBCore:Notify', src, 'You Sold Heroin', "success")   
-		end 
-	else
-		TriggerClientEvent('QBCore:Notify', src, 'You don\'t have the right items', "error") 
-	end
+			TriggerClientEvent('QBCore:Notify', src, 'you sold to the pusher', "success")  
+		else
+			Player.Functions.RemoveItem('heroin', 1)----change this
+			TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['heroin'], "remove")
+			Player.Functions.AddMoney("cash", Config.Pricesell-100, "sold-pawn-items")
+			TriggerClientEvent('QBCore:Notify', src, 'you sold to the pusher', "success")
+		end
 else
 	TriggerClientEvent('QBCore:Notify', src, 'You don\'t have the right items', "error") 
 	
 end
-end
 end)
 
-
-
 function CancelProcessing(playerId)
-	if playersProcessingCannabis[playerId] then
-		ClearTimeout(playersProcessingCannabis[playerId])
-		playersProcessingCannabis[playerId] = nil
+	if playersProcessingOpium[playerId] then
+		ClearTimeout(playersProcessingOpium[playerId])
+		playersProcessingOpium[playerId] = nil
 	end
 end
 
-RegisterServerEvent('qb-opium:cancelProcessing3')
-AddEventHandler('qb-opium:cancelProcessing3', function()
+RegisterServerEvent('qb-opiumpicking:cancelProcessing', function()
 	CancelProcessing(source)
 end)
 
@@ -82,8 +74,7 @@ AddEventHandler('QBCore_:playerDropped', function(playerId, reason)
 	CancelProcessing(playerId)
 end)
 
-RegisterServerEvent('qb-opium:onPlayerDeath2')
-AddEventHandler('qb-opium:onPlayerDeath2', function(data)
+RegisterServerEvent('qb-opiumpicking:onPlayerDeath', function(data)
 	local src = source
 	CancelProcessing(src)
 end)
@@ -92,13 +83,13 @@ QBCore.Functions.CreateCallback('poppy:process', function(source, cb)
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
 	 
-	if Player.PlayerData.item ~= nil and next(Player.PlayerData.items) ~= nil then
+	if Player.PlayerData.items ~= nil and next(Player.PlayerData.items) ~= nil then
 		for k, v in pairs(Player.PlayerData.items) do
 		    if Player.Playerdata.items[k] ~= nil then
-				if Player.Playerdata.items[k].name == "heroin" then
+				if Player.Playerdata.items[k].name == "opium" then
 					cb(true)
 			    else
-					TriggerClientEvent("QBCore:Notify", src, "You do not have any Heroin process", "error", 10000)
+					TriggerClientEvent("QBCore:Notify", src, "You do not have any Opium", "error", 10000)
 					cb(false)
 				end
 	        end
